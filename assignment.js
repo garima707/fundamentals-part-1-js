85b4c699eeeab1c3f0c0c38e95e5dc7faca7940f
Garima Jain <garimajain4752@gmail.com>
2021-08-30 16:26:07 +0530

    my first

---
+++ assignment.js
@@ -0,0 +1,56 @@
+/*
+let country = "India";
+let continent = "Asia";
+let population = 3;
+console.log(country);
+console.log(continent);
+console.log(population);*/
+/*const markheight = 1.69;
+const johnHeight = 1.95;
+const markMass = 78;
+const johnMass = 92;
+let BMIMark;
+let BMIJohn;
+BMIMark = markMass / markheight ** 2;
+BMIJohn = johnMass / (johnHeight * johnHeight);
+console.log(BMIMark, BMIJohn);
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`);
+}
+else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}`);
+}
+const dolphins = (96 + 108 + 89) / 3;
+const koalas = (88 + 91 + 110) / 3;
+if (dolphins > koalas) {
+    console.log("dolphins are the winner.");
+}
+else if (dolphins == koalas) {
+    console.log("draw match");
+}
+else {
+    console.log("koalas are the winner.");
+}
+
+const dolphins = (97 + 112 + 101) / 3;
+const koalas = (109 + 95 + 106) / 3;
+console.log(dolphins);
+console.log(koalas);
+if (dolphins > koalas && dolphins >= 100) {
+    console.log("dolphins are the winner.");
+}
+else if (dolphins == koalas && dolphins >= 100 && koalas >= 100) {
+    console.log("draw match");
+}
+else if (koalas > dolphins && koalas >= 100) {
+    console.log("koalas are the winner.");
+}
+else {
+    console.log("no winners!");
+}
+*/
+const bill = 430;
+const tip = bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+console.log(`the bill was ${bill} and tip was ${tip} and the total value  ${bill + tip}`);
+
+
